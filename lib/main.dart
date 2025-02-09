50f646268abaeebbb2e795993f27d4d25f2d9529
tsvbasa <tsvbasa7@gmail.com>
2025-02-09 15:55:42 -0500

    2025

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo_setstate/Pages/home_page.dart';
 import 'package:flutter_codigo_setstate/pages/general_page.dart';
+import 'package:flutter_codigo_setstate/pages/object_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'SetStateApp',
-      home: StatusGeneralPage(),
+      home: ObjectPage(),
     );
   }
 }
